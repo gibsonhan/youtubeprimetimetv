6f2e33ec3ffc1e56eb8dbd1018a65163f576863e
Gibson Han <gibsonxhan@gmail.com>
2021-08-30 14:47:56 +0700

    create function to check if data is empty, and create seperate module for youtube helper

--- pages/posts/test.tsx
+++ pages/posts/test.tsx
@@ -1,64 +1,50 @@
 import Link from 'next/link';
 import Script from 'next/script';
+import {
+    useEffect,
+    useState
+} from 'react';
 import { View } from 'react-native';
 
+//helpers
+import { isEmpty } from '../../utilities/isEmpty';
+import { isNotEmpty } from '../../utilities/isNotEmpty';
+import {
+    authenticate,
+    loadClient,
+    loadSubscription,
+    initYoutubeClient
+} from '../../utilities/youtubeHelper';
+
 const Test = (props: {}) => {
+    const [auth, setAuth] = useState({ })
+    const [subscription, setSubscription] = useState({ })
+
     async function authAndLoadClient() {
-        let authData = undefined
+        let authData = { }
         try {
             authData = await authenticate()
+            setAuth(authData)
             await loadClient()
         }
         catch (error) {
             console.error('Authenticate and Load Client failed', authAndLoadClient)
         }
+        setAuth(authData)
     }
-    async function authenticate() {
-        let auth = {}
-        try {
-            let auth2 = window.gapi.auth2
-            auth = await auth2.getAuthInstance().signIn({ scope: "https://www.googleapis.com/auth/youtube.readonly" })
-            console.log("Sign-in successful");
-        }
-        catch (error) {
-            console.error("Error signing in", error)
-        }
-        return auth
-    }
-    async function loadClient() {
-        try {
-            let client = window.gapi.client
-            client.setApiKey("AIzaSyD0IA4HnjdbmHGDl9_Q9fY0mlbTB_VzxRs");
-            await client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-            console.log("GAPI client loaded for API", loadClient);
-        }
-        catch (error) {
-            console.error("Error loading GAPI client for API", error);
-        }
-        return loadClient
-    }
-    // Make sure the client is loaded and sign-in is complete before calling this method.
-    async function execute() {
-        let list = {}
-        try {
-            //https://developers.google.com/youtube/v3/docs/subscriptions
-            let subscriptions = window.gapi.client.youtube.subscriptions
-            list = await subscriptions.list({
-                "part": [
-                    "contentDetails",
-                    "id",
-                    "snippet ",
-                    "subscriberSnippet"
-                ],
-                "mine": true,
-                "order": "unread"
-            })
-            console.log("subscriptions list", list);
-        }
-        catch (error) {
-            console.error("Execute error", error);
+
+    async function getSubscription() {
+        let list = await loadSubscription()
+        if (isNotEmpty(list)) {
+            setSubscription(list)
         }
     }
+
+
+    useEffect(() => {
+        console.log(auth)
+        console.log(typeof subscription, isEmpty(subscription), subscription)
+    }, [auth, subscription])
 
     return (
         <View>
@@ -66,32 +52,15 @@ const Test = (props: {}) => {
                 id="gapi"
                 src="https://apis.google.com/js/api.js"
                 strategy="beforeInteractive"
-                onLoad={() => {
-                    let gapi = window.gapi
-                    gapi.load("client:auth2", {
-                        callback: function () {
-                            gapi.auth2.init({
-                                client_id: "486025064243-8qhej8fb46i3fiird267mn1nrf43753g.apps.googleusercontent.com"
-                            });
-                            console.log('gapi.client loaded!')
-                        },
-                        onerror: function () {
-                            console.log('gapi.client failed to load!')
-                        },
-                        timeout: 5000,
-                        ontimeout: function () {
-                            console.log('gapi.client could not load in a timely manner')
-                        }
-                    });
-                }}
+                onLoad={() => { initYoutubeClient() }}
             />
             <div className="container mx-auto px-4">
                 <Link href="/">
                     <div>Home </div>
                 </Link>
-                <button onClick={() => authAndLoadClient()}>auth</button>
+                <button onClick={() => authAndLoadClient()}>Login</button>
                 <br />
-                <button onClick={() => execute()}>execute</button>
+                <button onClick={() => getSubscription()}>Get My Subscriptions</button>
             </div>
         </View>
     )
---
+++ utilities/isNotEmpty.tsx
@@ -0,0 +1,12 @@
+export function isNotEmpty(data: any) {
+    if (data === undefined) {
+        return false
+    }
+    else if (typeof data === "object") {
+        return Object.keys(data).length > 0;
+    }
+    else {
+        return data.length > 0;
+    }
+}
+
